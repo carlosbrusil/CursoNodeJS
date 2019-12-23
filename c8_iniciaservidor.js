0df3c60e8eb8b09b0759efd2d1ec089cf7e80f7a
Carlos Brusil <carlosbrusil@hotmail.com>
2019-12-22 23:10:31 -0500

    repaso en casa creacion servidor
    repasando en casa

--- c8_iniciaservidor.js
+++ c8_iniciaservidor.js
@@ -1,6 +1,7 @@
 const http=require('http');
 
 http.createServer(function (req,res){
-    res.write('<h1>creando servidor cbc</h1>');
+    res.writeHead(404,{'content-type':'text/html'});
+    res.write('<h1>creando servidor cbc</>');
     res.end;}
 ).listen(3000);
