724a847221c9534fe4462c12504be3bebefe8b7e
Carlos Brusil <carlosbrusil@hotmail.com>
2019-12-22 20:51:37 -0500

    inicio de servidor
    parte inicial basica para iniciar un servidor en nodejs

---
+++ c7_aplicavarios.js
@@ -0,0 +1,12 @@
+const fs=require('fs');
+//
+//
+let vec=[1,2, 3 ][4,5,6];
+console.log("");
+console.log("TABLA");
+for (let x=0;x<3;x++){
+    for (let y=0; y<2;y++){
+        console.log(vec[y]);
+    }
+}
+console.log("fin ");
