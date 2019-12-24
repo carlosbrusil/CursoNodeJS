6846ab64a31c3a7e0320c17982e35e7fb78c6d58
Carlos Brusil <carlosbrusil@hotmail.com>
2019-12-23 23:17:38 -0500

    un pino mas creando servidor
    definir la funcion llamada al crear el servidor como una constante fuera de la linea de crear servidor

--- c9_iniciaservidor2.js
+++ c9_iniciaservidor2.js
@@ -3,7 +3,18 @@
 const http=require('http');
 //
 // invocamos a la funcion para crear el servidor
-http.createServer(function (req,res){
+//
+// optimizamos el proceso crearservidor definiendo la funcion afuera
+const defineservidor=function (req,res)
+{
+    // definimos el tipo de respuesta que se dara
+    // a los diferentes CODIGOS DE ESTADO HTML
+    res.writeHead(200,{'content-type':'text/html'});
+    res.write('<h1>creando servidor tecnosig</>');
+    res.end;
+}
+/*
+    http.createServer(function (req,res){
     // definimos el tipo de respuesta que se dara
     // a los diferentes CODIGOS DE ESTADO HTML
     // en este caso 200 cuando el servidor ha respondido OK
@@ -11,3 +22,5 @@ http.createServer(function (req,res){
     res.write('<h1>creando servidor cbc</>');
     res.end;}
 ).listen(3000);// los resultados, mensajes y demas se escuchan por el puerto 3000
+*/
+http.createServer(defineservidor).listen(3000);
