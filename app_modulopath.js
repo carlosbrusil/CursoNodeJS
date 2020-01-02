c466a7121f4d1c8ed9f4f7e837895366119a5bab
Carlos Brusil <carlosbrusil@hotmail.com>
2020-01-01 22:24:03 -0500

    modulo readline parte 1
    aplicacion de una interface para hacer stdin y stdout mas sencillo en un solo metodo readline

--- app_modulopath.js
+++ app_modulopath.js
@@ -3,8 +3,12 @@ var util=require('util');
 var v8 = require('v8');
 var fuente=path.basename(__filename);
 var directorio=path.basename(__dirname);
-var direccion=util.format("La dieccion es: %10s / %10s",directorio,fuente);
+var direccion=util.format("La direccion es: %s / %s",directorio,fuente);
 //console.log("nombre del archivo fuente   "+path.basename(__filename));
 //console.log("nombre del directorio actual"+path.basename(__dirname));
 console.log(direccion);
-console.log(' estadisticas actuales \n',  v8.getHeapStatistics());
+//
+// util.log nos presenta ademas de la impresion de lo que esta en comillas 
+//      tambien genera la fecha y hora del sistema
+//      o "TIME STAMP"
+util.log(' estadisticas actuales \n',  v8.getHeapStatistics());
