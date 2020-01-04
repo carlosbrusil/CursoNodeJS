4a55ea5433d116b122dc7c575c88fea29521b05e
Carlos Brusil <carlosbrusil@hotmail.com>
2020-01-04 14:39:30 -0500

    Update app_moduloReadLine.js
    aplicacion del setprompt, prompt para expandir la inferface de entrada y salida con stdin, stdout

--- app_moduloReadLine.js
+++ app_moduloReadLine.js
@@ -1,6 +1,24 @@
 var readline=require('readline');
+var util=require('util');
 var rl=readline.createInterface(process.stdin,process.stdout);
-rl.question("Cual es tu nombre ",(respuesta)=>{
-    console.log(`Hola ${respuesta}`);
-    process.exit();
+var persona={
+        nombre:'',
+        observaciones:[]
+};
+var mensaje;
+rl.question("Cual es tu nombre ",(res1)=>{
+    persona.nombre=res1.trim();
+    console.log("Hola ",persona.nombre);
+    rl.setPrompt('Dime observacion ');
+    rl.prompt();
+});
+rl.on('line',(entrada)=>{
+    if (entrada.trim()==='salir'){
+        mensaje=util.format('nombre= %s observaciones= %j',persona.nombre,persona.observaciones);
+        console.log(mensaje);
+        process.exit();
+    }
+    rl.setPrompt('Siguiente observacion ');
+    rl.prompt();
+    persona.observaciones.push(entrada.trim());
 });
