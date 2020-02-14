feb29a8214f6295d32b70c52b4a53d05ddaf267c
Carlos Brusil <carlosbrusil@hotmail.com>
2020-02-13 22:03:17 -0500

    Calculadora en html css & js
    comenzando el diseño de la calculadora en html y css

---
+++ html-Calcu/funcionalidad.js
@@ -0,0 +1,9 @@
+function inicio(){
+    alert("hola mundo");
+    // variables
+    var resultado=document.getElementById("resultado");
+    var reset=document.getElementById("reset");
+    var igual=document.getElementById("igual");
+    var suma=document.getElementById("suma");
+    var uno=document.getElementById("uno");
+}
